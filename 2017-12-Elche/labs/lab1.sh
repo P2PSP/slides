656df5b4706f1439636e4b17d2d4d4fbe36e1c04
Your Name <you@example.com>
2017-11-26 15:41:15 +0100

    .

--- 2017-12-Elche/labs/lab1.sh
+++ 2017-12-Elche/labs/lab1.sh
@@ -3,9 +3,9 @@ killall vlc
 sleep 1
 
 echo "Creating the source"
-cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=http{dst=:8080/LBBB.ogv},dst=display}" --loop &
+cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=http{dst=:8080/BBB.ogv},dst=display}" --loop &
 sleep 5
 
 echo "Create two listeners"
-cvlc http://localhost:8080/LBBB.ogv &
-cvlc http://localhost:8080/LBBB.ogv &
+cvlc http://localhost:8080/BBB.ogv 2> /dev/null &
+cvlc http://localhost:8080/BBB.ogv 2> /dev/null &
--- 2017-12-Elche/labs/lab2.sh
+++ 2017-12-Elche/labs/lab2.sh
@@ -5,22 +5,20 @@ killall vlc
 sleep 1
 
 echo "Create two sources"
-cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv},dst=display}" --loop &
-#cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBBs.ogv}" --loop &
+#cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv},dst=display}" --loop &
+cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv}" --loop &
 sleep 1
-cvlc  ~/Videos/Lchi84_14_m4.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv},dst=display}" --loop &
-#cvlc ~/Videos/Lchi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/LLL.ogv}" --loop &
+#cvlc  ~/Videos/Lchi84_14_m4.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv},dst=display}" --loop &
+cvlc ~/Videos/Lchi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv}" --loop &
 sleep 1
-echo "Please, push <enter> to continue"
-read
 
 echo "Check the infrastructure"
 firefox http://localhost:8000 2> /dev/null &
 sleep 5
 
 echo "Create three listeners"
-cvlc http://localhost:8000/BBB.ogv 2> /dev/null &
-cvlc http://localhost:8000/BBB.ogv 2> /dev/null &
+mplayer http://localhost:8000/BBB.ogv 2> /dev/null &
+mplayer http://localhost:8000/BBB.ogv 2> /dev/null &
 cvlc http://localhost:8000/chi.ogv 2> /dev/null &
 
 set +x
