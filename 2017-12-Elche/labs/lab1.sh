04940498b3b06996e3303f2d6d130fd6ad29f119
Your Name <you@example.com>
2017-11-26 12:53:52 +0100

    .

--- 2017-12-Elche/labs/lab1.sh
+++ 2017-12-Elche/labs/lab1.sh
@@ -4,7 +4,7 @@ sleep 1
 
 echo "Creating the source"
 cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=http{dst=:8080/LBBB.ogv},dst=display}" --loop &
-sleep 1
+sleep 5
 
 echo "Create two listeners"
 cvlc http://localhost:8080/LBBB.ogv &
--- 2017-12-Elche/labs/lab2.sh
+++ 2017-12-Elche/labs/lab2.sh
@@ -1,18 +1,25 @@
+set -x
+
 echo "Killing all VLC instances (sources and listeners)"
 killall vlc
 sleep 1
 
 echo "Create two sources"
-cvlc ~/Videos/Big_Buck_Bunny_small.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBBs.ogv}" --loop &
+cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv},dst=display}" --loop &
+#cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBBs.ogv}" --loop &
 sleep 1
-cvlc ~/Videos/chi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/LLL.ogv}" --loop &
+cvlc  ~/Videos/Lchi84_14_m4.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv},dst=display}" --loop &
+#cvlc ~/Videos/Lchi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/LLL.ogv}" --loop &
 sleep 1
+read
 
 echo "Check the infrastructure"
 firefox http://localhost:8000 2> /dev/null &
 sleep 5
 
-echo "Create two listeners"
-cvlc http://localhost:8000/BBBs.ogv 2> /dev/null &
-cvlc http://localhost:8000/LLL.ogv 2> /dev/null &
+echo "Create three listeners"
+cvlc http://localhost:8000/BBB.ogv 2> /dev/null &
+cvlc http://localhost:8000/BBB.ogv 2> /dev/null &
+cvlc http://localhost:8000/chi.ogv 2> /dev/null &
 
+set +x
