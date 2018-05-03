e3783d98a6fbb8f5c7df06a8a18be75dfe1e3ee8
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-05-03 17:12:38 +0200

    auto detecting language added

--- index.php
+++ index.php
@@ -18,14 +18,22 @@
    
   <?php
    foreach (scandir(".",1) as $dir) {
-        if (substr($dir, 0,1) === "2" and file_exists($dir.'/index.html')) {
-  ?>
-		<a href="<?php echo $dir.'/index.html'; ?>">
+     if (substr($dir, 0,1) === "2" and file_exists($dir.'/index.html')) {
+         $slide_content = file_get_contents($dir."/index.html");
+         if (strpos($slide_content, 'lang="es"')){
+             $language = "🇪🇸";
+         }else{
+             $language = "🇬🇧";
+         }
+  ?>         
+                
+                <a href="<?php echo $dir.'/index.html'; ?>">
 		<div class="slide">
 		<img width="90%" src="<?php echo $dir.'/snapshot.png'; ?>">
 		<?php list($year, $month, $name) = explode("-", $dir); ?>
 		<h3><?php echo str_replace("_"," ",$name); ?></h3>
-		<span class="date"><?php echo $month."-".$year; ?></span>
+		<span class="lang"><?php echo "Language: ".$language; ?></span>
+		<span class="date"><?php echo " | Date: ".$month."-".$year; ?></span>
 		</div>
 		</a>
   <?php
