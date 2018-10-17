6dda388e79f551b32db3f5c57540c054f95cbadb
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-10-17 19:47:51 +0200

    .

--- 2018-11-Elche/labs/lab2.sh
+++ 2018-11-Elche/labs/lab2.sh
@@ -6,7 +6,7 @@ sleep 1
 
 echo "Create two sources"
 #cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv},dst=display}" --loop &
-cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv}" --loop &
+cvlc ~/Videos/LBBB.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/LBBB.ogv}" --loop &
 sleep 1
 #cvlc  ~/Videos/Lchi84_14_m4.ogv --sout "#duplicate{dst=std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv},dst=display}" --loop &
 cvlc ~/Videos/Lchi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv}" --loop &
