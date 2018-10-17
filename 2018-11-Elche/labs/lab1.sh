02aa64d2934497b0aa48f5da5218e21682fbc2a8
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-10-17 19:36:58 +0200

    .

--- 2018-11-Elche/labs/lab1.sh
+++ 2018-11-Elche/labs/lab1.sh
@@ -1,11 +1,13 @@
+# Lab 1: using vlc as a source an as a listener
+
 echo "Killing all VLC instances (sources and listeners)"
 killall vlc
 sleep 1
 
 echo "Creating the source"
-cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#duplicate{dst=http{dst=:8080/BBB.ogv},dst=display}" --loop &
+cvlc ~/Videos/LBBB.ogv --sout "#duplicate{dst=http{dst=:8080/LBBB.ogv},dst=display}" --loop &
 sleep 5
 
 echo "Create two listeners"
-cvlc http://localhost:8080/BBB.ogv 2> /dev/null &
-cvlc http://localhost:8080/BBB.ogv 2> /dev/null &
+cvlc http://localhost:8080/LBBB.ogv 2> /dev/null &
+cvlc http://localhost:8080/LBBB.ogv 2> /dev/null &
