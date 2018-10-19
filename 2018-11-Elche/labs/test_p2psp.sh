ae4603c3184ae0a3cff0d45c5c972d6c9d066f99
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-10-19 12:28:32 +0200

    .

--- 2018-11-Elche/labs/test_p2psp.sh
+++ 2018-11-Elche/labs/test_p2psp.sh
@@ -1,5 +1,3 @@
-# Lab 4: hybrid approach
-
 echo "Killing all VLC instances"
 killall vlc
 sleep 1
@@ -12,11 +10,11 @@ echo "Run a second Icecast2 server listening at port 9000"
 sleep 1
 
 echo "Feed all icecast servers (2 movies for 8000 and 1 for 9000)"
-cvlc ~/Videos/LBBB.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/LBBB.ogv}" --loop &
+cvlc ~/Videos/LBig_Buck_Bunny_small.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/BBB.ogv}" --loop &
 sleep 1
-cvlc ~/Videos/Lchi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/Lchi.ogv}" --loop &
+cvlc ~/Videos/Lchi84_14_m4.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:8000/chi.ogv}" --loop &
 sleep 1
-cvlc ~/Videos/Lhcil2003_01.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:9000/Lhcil.ogv}" --loop &
+cvlc ~/Videos/Lhcil2003_01.ogv --sout "#std{access=shout,mux=ogg,dst=source:hackme@localhost:9000/hcil.ogv}" --loop &
 sleep 1
 
 #echo "Check the infrastructure"
@@ -26,17 +24,17 @@ sleep 1
 #sleep 5
 
 echo "Run a listener connected to the master Icecast server"
-cvlc http://localhost:8000/LBBB.ogv 2> /dev/null &
+cvlc http://localhost:8000/BBB.ogv 2> /dev/null &
 sleep 1
 
 echo "Run a listener connected to the relay Icecast server"
-cvlc http://localhost:9000/Lhcil.ogv 2> /dev/null &
+cvlc http://localhost:9000/hcil.ogv 2> /dev/null &
 sleep 1
 
 echo "Create a P2PSP team"
-xterm -e "python ~/P2PSP/simulator/src/splitter_video.py" &
+xterm -e "~/P2PSP/p2psp-console/bin/splitter --source_addr 127.0.0.1 --source_port 8000 --splitter_port 8001 --channel BBB.ogv --header_size 30000" &
 sleep 1
-xterm -e "python ~/P2PSP/simulator/src/monitor --splitter_addr 127.0.0.1 --splitter_port 8001" &
+xterm -e "~/P2PSP/p2psp-console/bin/monitor --splitter_addr 127.0.0.1 --splitter_port 8001" &
 sleep 1
 cvlc http://localhost:9999 & # Monitor's player
 sleep 1
